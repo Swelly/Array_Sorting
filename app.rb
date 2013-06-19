715ae7f06fad22d9bbdf580cb5f39ba80fe6c2dd
Wesley Carr <surfswell17@gmail.com>
2013-06-19 17:48:19 -0400

    asking group size

--- app.rb
+++ app.rb
@@ -1,2 +1,10 @@
 students = ["Alan Appelstein", "Alice Mottola", "Alina Guzman", "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli", "Beryl Schragger", "Chris Clearfield", "Christine Coulter", "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong", "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Wesley Carr", "Zachariah Reitano"]
 
+group = []
+students.shuffle
+
+puts "How many members would you prefer in a group?"
+  group_size = gets.chomp.to_i
+
+group.push(students.pop(group_size))
+puts group
