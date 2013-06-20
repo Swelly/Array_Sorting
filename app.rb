cfeb428b4f674fa606e971447d3d79350aa5be63
Wesley Carr <surfswell17@gmail.com>
2013-06-19 23:25:24 -0400

    Finished program. added comments

--- app.rb
+++ app.rb
@@ -1,10 +1,24 @@
 students = ["Alan Appelstein", "Alice Mottola", "Alina Guzman", "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli", "Beryl Schragger", "Chris Clearfield", "Christine Coulter", "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong", "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Wesley Carr", "Zachariah Reitano"]
 
-group = []
-students.shuffle
-
 puts "How many members would you prefer in a group?"
-  group_size = gets.chomp.to_i
+  group_members = gets.chomp.to_i
 
-group.push(students.pop(group_size))
-puts group
+#Randomize students
+students.shuffle!
+
+total_groups = students.length / group_members
+#Create an empty group array for the random teams
+groups = []
+
+#Using .pop to populate groups[] from students
+total_groups.times do |goku|
+  groups << students.pop(group_members)
+end
+
+groups.each do |individuals|
+  puts individuals.to_s
+end
+#Must be a better way to add remaining student to a group array.
+if students != 0
+  puts "Oh no, #{students} needs to join a team"
+end
